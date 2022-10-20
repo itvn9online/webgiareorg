20ab5d33d580bcebcdc69a4222bad216bad879d0
itvn9online <itvn9online@gmail.com>
2022-10-20 13:00:02 +0700

    new cache method

--- app/Views/footer_cache.php
+++ app/Views/footer_cache.php
@@ -7,25 +7,25 @@ ob_end_clean();
 /*
  * Thay thế 1 số nội dung theo chuẩn chung
  */
-$main_content = str_replace( '="./"', '="' . DYNAMIC_BASE_URL . '"', $main_content );
+$main_content = str_replace('="./"', '="' . DYNAMIC_BASE_URL . '"', $main_content);
 
 //
 echo $main_content;
 
 // nếu không có ghi chú về việc tắt cache
-if ( WHY_EBCACHE_DISABLE == '' ) {
-    if ( ENABLE_EBCACHE != '' ) {
-        if ( function_exists( 'WGR_create_cache_file' ) ) {
-            $filename = WGR_create_cache_file();
+if (WHY_EBCACHE_DISABLE == '') {
+    if (ENABLE_EBCACHE != '') {
+        if (function_exists('WGR_get_cache_file')) {
+            $filename = WGR_get_cache_file();
             //echo $filename . '<br>' . "\n";
 
             //
-            if ( !WGR_cache_expire( $filename, EB_TIME_CACHE ) ) {
+            if (defined(EB_FALSE_CACHE) || WGR_my_cache($filename) === false) {
                 //echo __FILE__ . ':' . __LINE__ . '<br>' . "\n";
-                WGR_create_file( $filename, WGR_remove_html_empty_line( $main_content ) . WGR_builder_eb_cache_note( ENABLE_EBCACHE ) );
+                WGR_my_cache($filename, WGR_remove_html_empty_line($main_content) . WGR_builder_eb_cache_note(ENABLE_EBCACHE), EB_TIME_CACHE);
             }
         } else {
-            echo '<!-- WGR_create_cache_file not exists -->';
+            echo '<!-- WGR_get_cache_file not exists -->';
         }
     } else {
         echo '<!-- ENABLE_EBCACHE not define -->';
--- ebache.php
+++ ebache.php
@@ -5,27 +5,28 @@
 //echo session_id();
 
 // thời gian lưu cache
-defined( 'EB_TIME_CACHE' ) || define( 'EB_TIME_CACHE', 600 );
+defined('EB_TIME_CACHE') || define('EB_TIME_CACHE', 600);
 
 //
-$sub_dir_cache = [ 'ebcache' ];
-$sub_dir_cache[] = explode( ':', $_SERVER[ 'HTTP_HOST' ] )[ 0 ];
+$sub_dir_cache = ['ebcache'];
+$sub_dir_cache[] = explode(':', $_SERVER['HTTP_HOST'])[0];
 
 //
-if ( !function_exists( 'wp_is_mobile' ) ) {
+if (!function_exists('wp_is_mobile')) {
     // fake function wp_is_mobile of wordpress
-    function WGR_is_mobile() {
-        if ( empty( $_SERVER[ 'HTTP_USER_AGENT' ] ) ) {
+    function WGR_is_mobile()
+    {
+        if (empty($_SERVER['HTTP_USER_AGENT'])) {
             $is_mobile = false;
         }
         // Many mobile devices (all iPhone, iPad, etc.)
-        else if ( strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Mobile' ) !== false ||
-            strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Android' ) !== false ||
-            strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Silk/' ) !== false ||
-            strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Kindle' ) !== false ||
-            strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'BlackBerry' ) !== false ||
-            strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Opera Mini' ) !== false ||
-            strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Opera Mobi' ) !== false ) {
+        else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false ||
+            strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false ||
+            strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false ||
+            strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false ||
+            strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false ||
+            strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false ||
+            strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false) {
             $is_mobile = true;
         } else {
             $is_mobile = false;
@@ -36,58 +37,57 @@ if ( !function_exists( 'wp_is_mobile' ) ) {
     }
 
     //
-    if ( WGR_is_mobile() ) {
+    if (WGR_is_mobile()) {
         $sub_dir_cache[] = 'm';
     }
-} else if ( wp_is_mobile() ) {
+} else if (wp_is_mobile()) {
     $sub_dir_cache[] = 'm';
 }
 //print_r( $sub_dir_cache );
 
 // tự động tạo thư mục cache nếu chưa có
-$root_dir_cache = dirname( __DIR__ );
-foreach ( $sub_dir_cache as $v ) {
+$root_dir_cache = dirname(__DIR__);
+foreach ($sub_dir_cache as $v) {
     $root_dir_cache .= '/' . $v;
 
     //
-    if ( !is_dir( $root_dir_cache ) ) {
-        mkdir( $root_dir_cache, 0777 );
+    if (!is_dir($root_dir_cache)) {
+        mkdir($root_dir_cache, 0777);
         echo $root_dir_cache . '<br>' . "\n";
     }
 }
 
 // thư mục lưu ebcache
-define( 'EB_THEME_CACHE', $root_dir_cache . '/' );
+define('EB_THEME_CACHE', $root_dir_cache . '/');
 //echo EB_THEME_CACHE . '<br>' . "\n";
 //die( __FILE__ . ':' . __LINE__ );
 
 // nạp function tạo cache
 include_once __DIR__ . '/app/Cache/Global.php';
 
-// tạo tên file cache
-$filename = WGR_create_cache_file();
-//echo $filename . '<br>' . "\n";
-
 // chỉ cache với phương thức GET
-if ( $_SERVER[ 'REQUEST_METHOD' ] != 'GET' ) {
+if ($_SERVER['REQUEST_METHOD'] != 'GET') {
     // khác phát bỏ qua luôn
 }
 // tiếp tục kiểm tra cache
 else {
     // nếu tồn tại cookie wgr_ebsuppercache_timeout -> xem thời hạn của côkie còn không
-    $last_update_logeg_cache = isset( $_COOKIE[ 'wgr_ebsuppercache_timeout' ] ) ? $_COOKIE[ 'wgr_ebsuppercache_timeout' ] : 0;
+    $last_update_logeg_cache = isset($_COOKIE['wgr_ebsuppercache_timeout']) ? $_COOKIE['wgr_ebsuppercache_timeout'] : 0;
     //echo date( 'Y-m-d H:i:s', $last_update_logeg_cache ) . '<br>' . "\n";
     //echo $last_update_logeg_cache . '<br>' . "\n";
 
     // nếu còn hạn thì bỏ qua
-    if ( $last_update_logeg_cache > time() ) {
+    if ($last_update_logeg_cache > time()) {
         // đăng nhập rồi thì bỏ qua -> không nạp cache
         //echo 'wgr_ebsuppercache_timeout';
     }
     // nếu cache còn hiệu lức -> in ra luôn và thoát
-    else if ( WGR_cache_expire( $filename, EB_TIME_CACHE ) ) {
-        //echo __FILE__ . ':' . __LINE__ . '<br>' . "\n";
-        echo file_get_contents( $filename, 1 );
-        exit();
+    else {
+        $cache_content = WGR_my_cache(WGR_get_cache_file());
+        if ($cache_content !== false) {
+            echo $cache_content;
+            exit();
+        }
+        define('EB_FALSE_CACHE', 1);
     }
 }
