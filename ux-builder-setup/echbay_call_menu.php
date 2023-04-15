ac1cfcc927734d0f395cc984338e00a322e85add
Dao Quoc Dai <itvn9online@gmail.com>
2023-04-16 06:18:16 +0700

    cache for echbay menu

--- ux-builder-setup/echbay_call_menu.php
+++ ux-builder-setup/echbay_call_menu.php
@@ -2,18 +2,19 @@
 /*
  * Chức năng gọi tới các menu dựng sẵn của webgiareorg
  */
-function add_echbay_call_menu() {
+function add_echbay_call_menu()
+{
     $ops_list = [
         0 => '- Chọn menu -',
     ];
     $a = wp_get_nav_menus();
     //print_r( $a );
-    foreach ( $a as $k => $v ) {
-        $ops_list[ $v->term_id ] = $v->name;
+    foreach ($a as $k => $v) {
+        $ops_list[$v->term_id] = $v->name;
     }
 
     //
-    add_ux_builder_shortcode( 'echbay_call_menu', array(
+    add_ux_builder_shortcode('echbay_call_menu', array(
         'name' => 'Echbay Call Menu',
         'category' => 'Echbay',
         //'priority' => 1,
@@ -37,39 +38,58 @@ function add_echbay_call_menu() {
                 'placeholder' => 'Tùy chỉnh CSS',
             ),
         ),
-    ) );
+    ));
 }
-add_action( 'ux_builder_setup', 'add_echbay_call_menu' );
+add_action('ux_builder_setup', 'add_echbay_call_menu');
 
 // gọi short code từ UX Builder
-function action_echbay_call_menu( $atts ) {
-    extract( shortcode_atts( array(
+function action_echbay_call_menu($atts)
+{
+    extract(shortcode_atts(array(
         'call_menu' => '',
         'menu_title' => '',
         'custom_class' => '',
-    ), $atts ) );
+    ), $atts));
 
     //
-    if ( empty( $call_menu ) ) {
+    if (empty($call_menu)) {
         return __FUNCTION__ . ' call_menu is empty!';
     }
 
+    // sử dụng cache cho menu -> tránh duplicate query
+    $filename = '';
+    if (defined('EB_THEME_CACHE') && function_exists('WGR_get_cache_file')) {
+        $filename = EB_THEME_CACHE . __FUNCTION__ .  $call_menu . '.txt';
+
+        //
+        $html = WGR_my_cache($filename);
+        if ($html !== false) {
+            return $html;
+        }
+    }
+    //return $filename;
+
     //
-    $html = wp_nav_menu( [
+    $html = wp_nav_menu([
         'echo' => false, // không echo -> lấy kết quả trả về để return
         'menu' => $call_menu,
         'menu_class' => 'eb-set-menu-selected eb-menu cf',
-    ] );
+    ]);
 
-    if ( $menu_title != '' ) {
+    if ($menu_title != '') {
         $html = '<h4 class="echbay-flatsome-menu">' . $menu_title . '</h4>' . $html;
     }
 
-    if ( $custom_class != '' ) {
+    if ($custom_class != '') {
         $html = '<div class="' . $custom_class . '">' . $html . '</div>';
+    }
+
+    // lưu cache nếu có path cache file
+    if ($filename != '') {
+        WGR_my_cache($filename, $html);
     }
 
     //
     return $html;
 }
-add_shortcode( 'echbay_call_menu', 'action_echbay_call_menu' );
+add_shortcode('echbay_call_menu', 'action_echbay_call_menu');
