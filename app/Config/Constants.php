0ce7abfb8fbc1635839dc4122f338c001c03a426
itvn9online <itvn9online@gmail.com>
2023-05-27 05:14:33 +0700

    sync

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -1,24 +1,24 @@
 <?php
 
 // khi cần chuyển các file tĩnh sang url khác để giảm tải cho server chính thì dùng chức năng này
-defined( 'CDN_BASE_URL' ) || define( 'CDN_BASE_URL', '' );
+defined('CDN_BASE_URL') || define('CDN_BASE_URL', '');
 //echo CDN_BASE_URL . '<br>' . "\n";
 
 // thuộc tính này để xác định code áp dụng cho plugin wocomerce -> sẽ có 1 số tính năng bổ sung cho nó
-define( 'WGR_FOR_WOOCOMERCE', class_exists( 'WooCommerce' ) ? true : false );
+define('WGR_FOR_WOOCOMERCE', class_exists('WooCommerce') ? true : false);
 //echo WGR_FOR_WOOCOMERCE . '<br>' . "\n";
 
 // thời gian lưu cache
-defined( 'EB_TIME_CACHE' ) || define( 'EB_TIME_CACHE', 600 );
+defined('EB_TIME_CACHE') || define('EB_TIME_CACHE', 600);
 
 //
-if ( is_user_logged_in() ) {
-    define( 'USER_ID', get_current_user_id() );
+if (is_user_logged_in()) {
+    define('USER_ID', get_current_user_id());
 
     // duy trì cookie báo rằng đang đăng nhập -> duy trì 1 ngày để đảm bảo không bị chênh lệch múi giờ
-    setcookie( 'wgr_ebsuppercache_timeout', time() + EB_TIME_CACHE, time() + ( 24 * 3600 ), '/' );
+    setcookie('wgr_ebsuppercache_timeout', time() + EB_TIME_CACHE, time() + (24 * 3600), '/');
 } else {
-    define( 'USER_ID', 0 );
+    define('USER_ID', 0);
 }
 //echo USER_ID . '<br>' . "\n";
 
@@ -26,7 +26,8 @@ if ( is_user_logged_in() ) {
  * URL động cho website để có thể chạy trên nhiều tên miền khác nhau mà không cần config lại
  */
 // tinh chỉnh protocol theo ý thích -> mặc định là https
-defined( 'BASE_PROTOCOL' ) || define( 'BASE_PROTOCOL', 'https' );
+defined('BASE_PROTOCOL') || define('BASE_PROTOCOL', 'https');
 // -> url động cho website
-define( 'DYNAMIC_BASE_URL', BASE_PROTOCOL . '://' . $_SERVER[ 'HTTP_HOST' ] . '/' );
+//define('DYNAMIC_BASE_URL', BASE_PROTOCOL . '://' . $_SERVER['HTTP_HOST'] . '/');
+define('DYNAMIC_BASE_URL', get_home_url() . '/');
 //die( DYNAMIC_BASE_URL );
--- app/Models/Base.php
+++ app/Models/Base.php
@@ -1,14 +1,15 @@
 <?php
 
 // nạp CSS, JS để tránh phải bấm Ctrl + F5
-function WGR_get_add_css( $f, $ops = [], $attr = [] ) {
+function WGR_get_add_css($f, $ops = [], $attr = [])
+{
     //print_r( $ops );
     //echo $f . '<br>' . "\n";
-    $f = str_replace( ABSPATH, '', $f );
-    $f = ltrim( $f, '/' );
+    $f = str_replace(ABSPATH, '', $f);
+    $f = ltrim($f, '/');
     //echo $f . '<br>' . "\n";
 
-    if ( !file_exists( ABSPATH . $f ) ) {
+    if (!file_exists(ABSPATH . $f)) {
         return '<!-- ' . $f . ' not exist! -->';
     }
     //echo filesize( $f ) . '<br>' . "\n";
@@ -20,62 +21,67 @@ function WGR_get_add_css( $f, $ops = [], $attr = [] ) {
     */
 
     //
-    if ( isset( $ops[ 'get_content' ] ) ) {
-        return '<style>' . file_get_contents( $f, 1 ) . '</style>' . PHP_EOL;
+    if (isset($ops['get_content'])) {
+        return '<style>' . file_get_contents($f, 1) . '</style>' . PHP_EOL;
     }
 
     // xem có chạy qua CDN không -> có thì nó sẽ giảm tải cho server
-    if ( !isset( $ops[ 'cdn' ] ) ) {
-        $ops[ 'cdn' ] = '';
+    if (!isset($ops['cdn']) || $ops['cdn'] == '') {
+        $ops['cdn'] = DYNAMIC_BASE_URL;
     }
 
     //
-    if ( isset( $ops[ 'preload' ] ) ) {
+    if (isset($ops['preload'])) {
         $rel = 'rel="preload" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"';
     } else {
         $rel = 'rel="stylesheet" type="text/css" media="all"';
     }
-    return '<link ' . $rel . ' href="' . $ops[ 'cdn' ] . $f . '?v=' . filemtime( ABSPATH . $f ) . '"' . implode( ' ', $attr ) . ' />';
+    return '<link ' . $rel . ' href="' . $ops['cdn'] . $f . '?v=' . filemtime(ABSPATH . $f) . '"' . implode(' ', $attr) . ' />';
 }
 
 // chế độ nạp css thông thường
-function WGR_add_css( $f, $ops = [], $attr = [] ) {
-    echo WGR_get_add_css( $f, $ops, $attr ) . PHP_EOL;
+function WGR_add_css($f, $ops = [], $attr = [])
+{
+    echo WGR_get_add_css($f, $ops, $attr) . PHP_EOL;
 }
 
 // thêm nhiều file cùng 1 thuộc tính
-function WGR_adds_css( $fs, $ops = [], $attr = [] ) {
-    foreach ( $fs as $f ) {
-        echo WGR_get_add_css( $f, $ops, $attr ) . PHP_EOL;
+function WGR_adds_css($fs, $ops = [], $attr = [])
+{
+    foreach ($fs as $f) {
+        echo WGR_get_add_css($f, $ops, $attr) . PHP_EOL;
     }
 }
 
 // chế độ nạp trước css
-function WGR_preload_css( $f, $ops = [] ) {
-    $ops[ 'preload' ] = 1;
+function WGR_preload_css($f, $ops = [])
+{
+    $ops['preload'] = 1;
 
     //
-    echo WGR_get_add_css( $f, $ops ) . PHP_EOL;
+    echo WGR_get_add_css($f, $ops) . PHP_EOL;
 }
 
-function WGR_preloads_css( $fs, $ops = [] ) {
-    $ops[ 'preload' ] = 1;
+function WGR_preloads_css($fs, $ops = [])
+{
+    $ops['preload'] = 1;
 
     //
-    foreach ( $fs as $f ) {
-        echo WGR_get_add_css( $f, $ops ) . PHP_EOL;
+    foreach ($fs as $f) {
+        echo WGR_get_add_css($f, $ops) . PHP_EOL;
     }
 }
 
 /*
  * add javascript
  */
-function WGR_get_add_js( $f, $ops = [], $attr = [] ) {
+function WGR_get_add_js($f, $ops = [], $attr = [])
+{
     //print_r( $ops );
-    $f = str_replace( ABSPATH, '', $f );
-    $f = ltrim( $f, '/' );
+    $f = str_replace(ABSPATH, '', $f);
+    $f = ltrim($f, '/');
     //echo $f . '<br>' . "\n";
-    if ( !file_exists( ABSPATH . $f ) ) {
+    if (!file_exists(ABSPATH . $f)) {
         return '<!-- ' . $f . ' not exist! -->';
     }
     //echo filesize( $f ) . '<br>' . "\n";
@@ -87,52 +93,57 @@ function WGR_get_add_js( $f, $ops = [], $attr = [] ) {
     */
 
     //
-    if ( isset( $ops[ 'get_content' ] ) ) {
-        return '<script>' . file_get_contents( $f, 1 ) . '</script>';
+    if (isset($ops['get_content'])) {
+        return '<script>' . file_get_contents($f, 1) . '</script>';
     }
 
     // xem có chạy qua CDN không -> có thì nó sẽ giảm tải cho server
-    if ( !isset( $ops[ 'cdn' ] ) ) {
-        $ops[ 'cdn' ] = '';
+    if (!isset($ops['cdn']) || $ops['cdn'] == '') {
+        $ops['cdn'] = DYNAMIC_BASE_URL;
     }
 
     //
-    if ( isset( $ops[ 'preload' ] ) ) {
-        return '<link rel="preload" as="script" href="' . $ops[ 'cdn' ] . $f . '?v=' . filemtime( ABSPATH . $f ) . '">';
+    if (isset($ops['preload'])) {
+        return '<link rel="preload" as="script" href="' . $ops['cdn'] . $f . '?v=' . filemtime(ABSPATH . $f) . '">';
     }
     //print_r( $attr );
-    return '<script src="' . $ops[ 'cdn' ] . $f . '?v=' . filemtime( ABSPATH . $f ) . '" ' . implode( ' ', $attr ) . '></script>';
+    return '<script src="' . $ops['cdn'] . $f . '?v=' . filemtime(ABSPATH . $f) . '" ' . implode(' ', $attr) . '></script>';
 }
 
 // thêm 1 file
-function WGR_add_js( $f, $ops = [], $attr = [] ) {
-    echo WGR_get_add_js( $f, $ops, $attr ) . PHP_EOL;
+function WGR_add_js($f, $ops = [], $attr = [])
+{
+    echo WGR_get_add_js($f, $ops, $attr) . PHP_EOL;
 }
 // thêm nhiều file cùng 1 thuộc tính
-function WGR_adds_js( $fs, $ops = [], $attr = [] ) {
-    foreach ( $fs as $f ) {
-        echo WGR_get_add_js( $f, $ops, $attr ) . PHP_EOL;
+function WGR_adds_js($fs, $ops = [], $attr = [])
+{
+    foreach ($fs as $f) {
+        echo WGR_get_add_js($f, $ops, $attr) . PHP_EOL;
     }
 }
 // chế độ nạp trước css
-function WGR_preload_js( $f, $ops = [] ) {
-    $ops[ 'preload' ] = 1;
+function WGR_preload_js($f, $ops = [])
+{
+    $ops['preload'] = 1;
 
     //
-    echo WGR_get_add_js( $f, $ops ) . PHP_EOL;
+    echo WGR_get_add_js($f, $ops) . PHP_EOL;
 }
 
-function WGR_preloads_js( $fs, $ops = [] ) {
-    $ops[ 'preload' ] = 1;
+function WGR_preloads_js($fs, $ops = [])
+{
+    $ops['preload'] = 1;
 
     //
-    foreach ( $fs as $f ) {
-        echo WGR_get_add_js( $f, $ops ) . PHP_EOL;
+    foreach ($fs as $f) {
+        echo WGR_get_add_js($f, $ops) . PHP_EOL;
     }
 }
 
 //
-function WGR_select( $sql ) {
+function WGR_select($sql)
+{
     global $wpdb;
-    return $wpdb->get_results( trim( $sql ), OBJECT );
+    return $wpdb->get_results(trim($sql), OBJECT);
 }
