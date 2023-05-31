e66e2856965dee4421ccc7b3f983532305643025
itvn9online <itvn9online@gmail.com>
2023-06-01 00:19:31 +0700

    sync

--- app/Admin/autoload/function-update.php
+++ app/Admin/autoload/function-update.php
@@ -3,6 +3,25 @@
 * Kiểm tra và cập nhật lại nội dung cho file update của flatsome -> sử dụng chức năng update do webgiare cung cấp
 */
 
+function WGR_flatsome_function_update($f)
+{
+    //echo $f . PHP_EOL;
+    // nếu còn tồn tại chuỗi _site_transient_update_themes -> vẫn còn đang dùng code của flatsome
+    if (
+        file_exists($f)
+        && strpos(file_get_contents($f), 'webgiare_v3_update_themes') === false
+    ) {
+        //die(__FILE__ . ':' . __LINE__);
+        echo $f . PHP_EOL;
+
+        // copy 1 bản backup
+        copy($f, str_replace('/function-update.php', '/function-update-flatsome.php', $f));
+
+        // copy file mẫu ghi đè vào file của flatsome
+        copy(WGR_BASE_PATH . 'function-update.php', $f);
+    }
+}
+
 //
 if (!in_array(
     $_SERVER['HTTP_HOST'],
@@ -12,17 +31,5 @@ if (!in_array(
         'world.webgiare.org',
     ]
 )) {
-    $flatsome_function_update = dirname(WGR_CHILD_PATH) . '/flatsome/inc/functions/function-update.php';
-    //echo $flatsome_function_update . PHP_EOL;
-    // nếu còn tồn tại chuỗi _site_transient_update_themes -> vẫn còn đang dùng code của flatsome
-    if (
-        file_exists($flatsome_function_update)
-        && strpos(file_get_contents($flatsome_function_update), 'webgiare_v3_update_themes') === false
-    ) {
-        //die(__FILE__ . ':' . __LINE__);
-        echo $flatsome_function_update . PHP_EOL;
-
-        // copy file mẫu ghi đè vào file của flatsome
-        copy(WGR_BASE_PATH . 'function-update.php', $flatsome_function_update);
-    }
+    WGR_flatsome_function_update(dirname(WGR_CHILD_PATH) . '/flatsome/inc/functions/function-update.php');
 }
