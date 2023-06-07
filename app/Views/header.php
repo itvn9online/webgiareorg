d6c4a0bb22c0ac3adbbfeb2c825a431914f75cba
itvn9online <itvn9online@gmail.com>
2023-06-08 00:02:06 +0700

    sync

--- app/Views/header.php
+++ app/Views/header.php
@@ -27,35 +27,42 @@ include __DIR__ . '/header_cache.php';
     <?php
 
     // đặt tham số này để không nạp lại base nữa
-//define( 'HAS_BASE_HREF', true );
-    
+    //define( 'HAS_BASE_HREF', true );
+
     // nạp phần css inline để phục vụ cho bản mobile
     ?>
     <style>
-    <?php echo file_get_contents(WGR_BASE_PATH . 'public/css/mobile-usability.css', 1);
-    ?>
+        <?php echo file_get_contents(WGR_BASE_PATH . 'public/css/mobile-usability.css', 1);
+        ?>
     </style>
     <?php
 
     //
     wp_head();
 
-    // nạp file font theo kiểu inline
-    $font_awesome_before = WGR_get_add_css(WGR_BASE_PATH . 'public/thirdparty/awesome47/css/font-awesome.before.css', [
-        'get_content' => 1
-    ]);
-    $font_awesome_before = str_replace('../fonts/', WGR_BASE_URI . 'public/thirdparty/awesome47/fonts/', $font_awesome_before);
-    echo $font_awesome_before;
+    // nếu có lệnh này trong child-theme -> sẽ add fontawesome 4
+    if (defined('ADD_FONT_AWESOME4')) {
+        // nạp file font theo kiểu inline
+        $font_awesome_before = WGR_get_add_css(WGR_BASE_PATH . 'public/thirdparty/awesome47/css/font-awesome.before.css', [
+            'get_content' => 1
+        ]);
+        $font_awesome_before = str_replace('../fonts/', WGR_BASE_URI . 'public/thirdparty/awesome47/fonts/', $font_awesome_before);
+        echo $font_awesome_before;
+    } else {
+    ?>
+        <!-- Font Awesome4 disable by webgiareorg default -->
+        <?php
+    }
 
 
     // nạp một số css ở dạng preload
     $arr_preload_bootstrap = [
-        CDN_BASE_URL . WGR_BASE_URI . 'public/thirdparty/awesome47/css/font-awesome.min.css?v=4.7',
+        defined('ADD_FONT_AWESOME4') ? CDN_BASE_URL . WGR_BASE_URI . 'public/thirdparty/awesome47/css/font-awesome.min.css?v=4.7' : '',
 
         // bản full
         //CDN_BASE_URL . 'thirdparty/bootstrap-5.1.3/css/bootstrap.min.css',
         //'thirdparty/bootstrap-5.1.3/css/bootstrap.rtl.min.css',
-    
+
         // các module đơn lẻ
         //'thirdparty/bootstrap-5.1.3/css/bootstrap-grid.min.css',
         //'thirdparty/bootstrap-5.1.3/css/bootstrap-grid.rtl.min.css',
@@ -63,15 +70,17 @@ include __DIR__ . '/header_cache.php';
         //'thirdparty/bootstrap-5.1.3/css/bootstrap-reboot.rtl.min.css',
         //'thirdparty/bootstrap-5.1.3/css/bootstrap-utilities.min.css',
         //'thirdparty/bootstrap-5.1.3/css/bootstrap-utilities.rtl.min.css',
-    
+
         //
         CDN_BASE_URL . WGR_BASE_URI . 'public/css/d.css?v=' . filemtime(WGR_BASE_PATH . 'public/css/d.css'),
     ];
 
     foreach ($arr_preload_bootstrap as $v) {
-    ?>
-    <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="<?php echo $v; ?>" />
+        if ($v != '') {
+        ?>
+            <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="<?php echo $v; ?>" />
     <?php
+        }
     }
 
 
@@ -81,24 +90,24 @@ include __DIR__ . '/header_cache.php';
         //WGR_BASE_PATH . 'public/css/d.css',
         WGR_CHILD_PATH . 'css/d.css',
     ], [
-            'cdn' => CDN_BASE_URL,
-        ]);
+        'cdn' => CDN_BASE_URL,
+    ]);
 
     // nạp css dùng cho từng loại trang
     if (is_home() || is_front_page()) {
         WGR_adds_css([
             WGR_CHILD_PATH . 'css/home.css',
         ], [
-                'cdn' => CDN_BASE_URL,
-            ]);
+            'cdn' => CDN_BASE_URL,
+        ]);
     }
 
     // các file js bắt buộc phải nạp trước
     WGR_adds_js([
         WGR_BASE_PATH . 'public/javascript/functions.js',
     ], [
-            'cdn' => CDN_BASE_URL,
-        ]);
+        'cdn' => CDN_BASE_URL,
+    ]);
 
     ?>
 </head>
@@ -117,23 +126,23 @@ include __DIR__ . '/header_cache.php';
          */
         if (file_exists(WGR_CHILD_PATH . 'Views/top.php')) {
         ?>
-        <header id="header" class="header-wrapper wgr-primary-header">
-            <?php
-            include WGR_CHILD_PATH . 'Views/top.php';
-            ?>
-        </header>
+            <header id="header" class="header-wrapper wgr-primary-header">
+                <?php
+                include WGR_CHILD_PATH . 'Views/top.php';
+                ?>
+            </header>
         <?php
         }
         /*
          * không thì dùng top của flatsome
-         */else {
+         */ else {
             do_action('flatsome_before_header');
         ?>
-        <header id="header" class="header flatsome-primary-header <?php flatsome_header_classes(); ?>">
-            <div class="header-wrapper">
-                <?php get_template_part('template-parts/header/header', 'wrapper'); ?>
-            </div>
-        </header>
+            <header id="header" class="header flatsome-primary-header <?php flatsome_header_classes(); ?>">
+                <div class="header-wrapper">
+                    <?php get_template_part('template-parts/header/header', 'wrapper'); ?>
+                </div>
+            </header>
         <?php
             do_action('flatsome_after_header');
         }
--- themes/functions-sample.php
+++ themes/functions-sample.php
@@ -6,6 +6,9 @@
 //
 define('WGR_CHILD_PATH', __DIR__ . '/');
 
+// Nếu muốn dùng font-awesome4 thì đặt lệnh này -> không thì thôi -> mặc định không cài -> cho nhẹ web
+//define('ADD_FONT_AWESOME4', true);
+
 // nạp thư viện webgiare
 include dirname(dirname(__DIR__)) . '/webgiareorg/' . 'functions.php';
 //echo WGR_BASE_PATH;
