868d3ceb95532ea1a6fd53417b3b079ba995e817
itvn9online <itvn9online@gmail.com>
2023-08-17 18:08:50 +0700

    sync

--- eb_cache.php
+++ eb_cache.php
@@ -9,29 +9,28 @@ defined('EB_TIME_CACHE') || define('EB_TIME_CACHE', 300);
 
 //
 $sub_dir_cache = ['ebcache'];
-$sub_dir_cache[] = explode(':', str_replace('www.', '', $_SERVER['HTTP_HOST']))[0];
+$cache_prefix = str_replace('www.', '', str_replace('.', '', str_replace('-', '_', explode(':', $_SERVER['HTTP_HOST'])[0])));
 
 //
 if (!function_exists('wp_is_mobile')) {
     // fake function wp_is_mobile of wordpress
     function WGR_is_mobile()
     {
-        if (empty($_SERVER['HTTP_USER_AGENT'])) {
-            $is_mobile = false;
-        }
-        // Many mobile devices (all iPhone, iPad, etc.)
-        else if (
-            strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false ||
-            strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false ||
-            strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false ||
-            strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false ||
-            strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false ||
-            strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false ||
-            strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false
-        ) {
-            $is_mobile = true;
-        } else {
-            $is_mobile = false;
+        $is_mobile = false;
+        if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+            $a = $_SERVER['HTTP_USER_AGENT'];
+            if (
+                // Many mobile devices (all iPhone, iPad, etc.)
+                strpos($a, 'Mobile') !== false ||
+                strpos($a, 'Android') !== false ||
+                strpos($a, 'Silk/') !== false ||
+                strpos($a, 'Kindle') !== false ||
+                strpos($a, 'BlackBerry') !== false ||
+                strpos($a, 'Opera Mini') !== false ||
+                strpos($a, 'Opera Mobi') !== false
+            ) {
+                $is_mobile = true;
+            }
         }
 
         //
@@ -40,11 +39,12 @@ if (!function_exists('wp_is_mobile')) {
 
     //
     if (WGR_is_mobile()) {
-        $sub_dir_cache[] = 'm';
+        $cache_prefix .= '_m';
     }
 } else if (wp_is_mobile()) {
-    $sub_dir_cache[] = 'm';
+    $cache_prefix .= '_m';
 }
+$sub_dir_cache[] = $cache_prefix;
 //print_r($sub_dir_cache);
 
 // tự động tạo thư mục cache nếu chưa có
