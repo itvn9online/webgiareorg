36681733fcb92d2d575dcadb0000ca3880d71acb
itvn9online <itvn9online@gmail.com>
2024-09-28 02:52:45 +0700

    sync

--- app/Admin/menu/about.php
+++ app/Admin/menu/about.php
@@ -54,7 +54,7 @@ if (is_file($path_htaccess)) {
 function check_and_update_webgiareorg()
 {
     $version = file_get_contents(WGR_BASE_PATH . 'VERSION');
-    $remove_version = file_get_contents('https://raw.githubusercontent.com/itvn9online/webgiareorg/main/VERSION');
+    $remove_version = WGR_get_contents('https://raw.githubusercontent.com/itvn9online/webgiareorg/main/VERSION');
     // echo EB_MY_CACHE_CONFIG . '<br>' . PHP_EOL;
     // echo EB_THEME_CACHE . '<br>' . PHP_EOL;
 
--- app/Models/Base.php
+++ app/Models/Base.php
@@ -148,3 +148,31 @@ function WGR_select($sql)
     global $wpdb;
     return $wpdb->get_results(trim($sql), OBJECT);
 }
+
+// hàm file_get_contents khó chạy trên localhost -> dùng cURL để thay thế
+function WGR_get_contents($url, $flag = 0)
+{
+    if ($flag > 0) {
+        return file_get_contents($url, 1);
+    }
+
+    // 
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    // echo $response;
+    return $response;
+}
