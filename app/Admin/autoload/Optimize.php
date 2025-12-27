593c3c5db9c640cdf96baa114ab9bbffdfe68c24
itvn9online <itvn9online@gmail.com>
2025-12-27 22:28:51 +0700

    sync

--- app/Admin/autoload/Optimize.php
+++ app/Admin/autoload/Optimize.php
@@ -622,12 +622,14 @@ function WGR_list_backup_css_js()
     }
 
     // 
-    WGR_list_backup_css(WGR_BASE_PATH . 'public', 'css', 'css.bak');
-    WGR_list_backup_js(WGR_BASE_PATH . 'public', 'javascript', 'js.bak');
+    WGR_list_backup_css(WGR_BASE_PATH . 'public');
+    WGR_list_backup_js(WGR_BASE_PATH . 'public');
 
     // css, js của từng theme
-    WGR_list_backup_css(WGR_CHILD_PATH, 'css', 'css.bak');
-    WGR_list_backup_js(WGR_CHILD_PATH, 'javascript', 'js.bak');
+    WGR_list_backup_css(WGR_CHILD_PATH);
+    WGR_list_backup_css(WGR_CHILD_PATH, 'ui');
+    WGR_list_backup_js(WGR_CHILD_PATH);
+    WGR_list_backup_js(WGR_CHILD_PATH, 'ui');
 }
 
 /**
@@ -661,7 +663,9 @@ function WGR_optimize_css_js()
 
     // css, js của từng theme
     WGR_optimize_action_css(WGR_CHILD_PATH);
+    WGR_optimize_action_css(WGR_CHILD_PATH, 'ui');
     WGR_optimize_action_js(WGR_CHILD_PATH);
+    WGR_optimize_action_js(WGR_CHILD_PATH, 'ui');
 
     // optimize phần view -> optimize HTML
     if (WGR_optimize_action_views(WGR_BASE_PATH . 'app') === true) {
